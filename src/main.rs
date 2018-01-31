20c87e3c2bd056471396b0d4d971f7551a96b2a3
Russell Hay <russell.hay@gmail.com>
2018-01-30 20:41:07 -0800

    Checkpoint from day one of streaming

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#![feature(conservative_impl_trait)]
 #![allow(dead_code)]
 #![allow(unused_imports)]
 extern crate camera_capture;
 extern crate clap;
 extern crate chrono;
+extern crate tokio_timer;
+extern crate futures;
+extern crate image;
 
 use std::path::Path;
 use std::fs::File;
@@ -10,7 +14,22 @@ use clap::{App, Arg};
 use chrono::prelude::*;
 use std::time;
 use std::error::Error;
+use tokio_timer::Timer;
+use futures::{Future, future};
+use futures::future::FutureResult;
+use camera_capture::ImageIterator;
 
+type Image = image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>>;
+
+// struct ImageIterator {
+
+// }
+
+// impl ImageIterator {
+//     fn next(&mut self) -> Option<Image> {
+//         None
+//     }
+// }
 
 fn until_time(from_date: &DateTime<Local>) -> time::Duration {
     let now = Local::now();
@@ -55,15 +74,41 @@ fn main() {
             )
         .get_matches();
     
+    let cam_device_number = matches.value_of("device").unwrap().parse::<u32>().unwrap();
     let template = template_from_str(matches.value_of("time").unwrap()).unwrap();
     let next_run = next_run_time(&template);
     let sleep_duration = until_time(&next_run);
-    println!("{:?} => {:?} ({:?})", template, next_run, sleep_duration);
+    println!("Sleep Duration = {:?}", sleep_duration);
+
+    let timer = Timer::default();
+
+    let wait_on_picture_time = timer.sleep(sleep_duration);
     
-    // let expressive_duration = chrono::Duration::from_std(sleep_duration);
-    // println!("{:?}", expressive_duration.unwrap().num_hours());
-    // let cam = camera_capture::create(2).unwrap();
-    // let mut cam = cam.fps(1.0).unwrap().resolution(1920, 1080).unwrap().start().unwrap();
+    fn camera_start(device: u32) -> impl Future<Item=ImageIterator, Error=()> {
+        // future::ok(ImageIterator {})
+        future::ok(camera_capture::create(device).unwrap()
+            .fps(1.0).unwrap()
+            .resolution(1920, 1080).unwrap()
+            .start().unwrap())
+    }
+    fn take_picture(cam: Result<ImageIterator, ()>) -> impl Future<Item=Option<Image>, Error=()> {
+        future::ok(cam.unwrap().next())
+    }
+    let camera_warmup = timer.sleep(time::Duration::from_secs(10)); 
+    let capture_chain = wait_on_picture_time
+        .then(|_| camera_start(cam_device_number))
+        .then(|res| {
+            let _ = camera_warmup.wait();
+            res
+        })
+        ;
+
+    match capture_chain.wait() {
+        Err(e) => println!("Failed! {:?}", e),
+        _ => println!("Chain Done")
+        
+    };
+
     // let img = cam.next().unwrap();
 
     // let filename = "test.png";
